d1b07fdd55d42f6d7f5db96af744c5804100a187
thansex000 <loclop3@gmail.com>
2024-10-10 08:20:43 +0700

    tien

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        <footer>
-            chân trang
-        </footer>
-    </div>
-</template>
-
-<script>
-export default {
-    name: 'footerComponent'
-}
-</script>
-
-<style></style>
+    <footer class="bg-gray-500 text-white text-center py-4 mt-auto w-full">
+      <div class="container mx-auto">
+        <p>© 2024 GizmoShop Admin Dashboard. All rights reserved.</p>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FooterComponent'
+  }
+  </script>
+  
+  <style scoped>
+  footer {
+    position: fixed;
+    bottom: 0;
+  }
+  </style>
+  
