d9d9e7402c5281aee78f9b754ce67f5b1b1e28f1
haihaycode <kienthuc177@gmail.com>
2024-10-10 14:20:57 +0700

    dsgf

--- src/api/config.js
+++ src/api/config.js
@@ -4,5 +4,5 @@ export const SUCCESS_CODE = 200
 
 export const DEV = process.env.NODE_ENV !== 'production';
 
-export const HOST = DEV ? 'http://localhost:8081' : 'http://localhost:8081';
+export const HOST = DEV ? 'http://localhost:8081' : 'https://gizmoshop-latest.onrender.com/';
 // https://gizmoshop-latest.onrender.com/
