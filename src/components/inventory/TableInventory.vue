a8bdc54384681009a3474505c77f4bb17d0acedf
haihaycode <kienthuc177@gmail.com>
2024-10-13 15:46:16 +0700

    update inventory

--- src/components/inventory/TableInventory.vue
+++ src/components/inventory/TableInventory.vue
@@ -10,13 +10,16 @@
                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Xã</th>
                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Kinh độ</th>
                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Vĩ độ</th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Trạng thái</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Trạng thái
+                </th>
             </template>
             <!-- Body Slot -->
             <template #body>
-                <tr v-for="(item, index) in inventoryList" :key="index" class="hover:bg-gray-300" >
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{index + 1}}</td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ item.inventoryName }}</td>
+                <tr v-for="(item, index) in inventoryList" :key="index" class="hover:bg-gray-300"
+                    @click="updateInventoryModal(item.id)">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ index + 1 }}</td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ item.inventoryName }}
+                    </td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.city }}</td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.district }}</td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.commune }}</td>
@@ -31,32 +34,53 @@
                 <td colspan="5" class="px-6 py-3 text-sm text-gray-700">Total: {{ inventoryList.length }} entries</td>
             </template> -->
         </TableComponent>
+
+
+        <updateInventory v-if="idInventorySelected" :id="idInventorySelected" :isOpen="ModalUpdateInventoryIsOpen"
+            @close="updateInventoryModal" @update-success="getListInventory">
+        </updateInventory>
+
     </div>
 </template>
 
 <script>
 import { listInventory } from '@/api/inventoryApi';
 import TableComponent from '../table/TableComponent.vue';
-
+import updateInventory from './updateInventory.vue';
 export default {
     name: 'TableInventoryComponent',
     components: {
         TableComponent,
+        updateInventory
     },
     data() {
         return {
+            ModalUpdateInventoryIsOpen: false,
+            idInventorySelected: null,
             inventoryList: [],
+
         };
     },
     async created() {
-        try {
-            const response = await listInventory();
-            this.inventoryList = response.data.content;
-            console.log(this.inventoryList)
-        } catch (error) {
-            console.error('Error loading inventory list:', error);
-        }
+        this.getListInventory();
     },
+    methods: {
+
+        async getListInventory() {
+            try {
+                const response = await listInventory();
+                this.inventoryList = response.data.content;
+                console.log(this.inventoryList)
+            } catch (error) {
+                console.error('Error loading inventory list:', error);
+            }
+        },
+
+        updateInventoryModal(idInventory) {
+            this.idInventorySelected = idInventory;
+            this.ModalUpdateInventoryIsOpen = !this.ModalUpdateInventoryIsOpen;
+        }
+    }
 };
 </script>
 
