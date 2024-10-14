c49de67d352a01ef6a3c5dde95f6726add4320b0
haihaycode <kienthuc177@gmail.com>
2024-10-14 18:18:38 +0700

    update role account

--- src/components/staff/listStaffComponent.vue
+++ src/components/staff/listStaffComponent.vue
@@ -3,20 +3,39 @@
     <TableComponent>
       <!-- Header Slot -->
       <template #header>
-        <th @click="changeSort('id')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Stt <span v-html="getSortIcon('id')"></span></th>
-        <th @click="changeSort('fullname')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Tên Nhân Viên <span v-html="getSortIcon('inventoryName')"></span></th>
-        <th @click="changeSort('email')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Email <span v-html="getSortIcon('city')"></span></th>
-        <th @click="changeSort('sdt')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Số điện thoại <span v-html="getSortIcon('district')"></span></th>
-        <th @click="changeSort('birthday')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Năm sinh<span v-html="getSortIcon('ward')"></span></th>
-        <th @click="changeSort('extra_info')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Thông tin thêm <span v-html="getSortIcon('longitude')"></span></th>
-        <th @click="changeSort('extra_info')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Quyền <span v-html="getSortIcon('longitude')"></span></th>
-        <th @click="changeSort('formattedCreateAt')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Ngày khởi tạo <span v-html="getSortIcon('latitude')"></span></th>
-        <th @click="changeSort('deleted')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Trạng thái <span v-html="getSortIcon('active')"></span></th>
+        <th @click="changeSort('id')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Stt <span
+            v-html="getSortIcon('id')"></span></th>
+        <th @click="changeSort('fullname')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Tên Nhân Viên <span
+            v-html="getSortIcon('inventoryName')"></span></th>
+        <th @click="changeSort('email')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Email <span
+            v-html="getSortIcon('city')"></span></th>
+        <th @click="changeSort('sdt')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Số điện thoại <span
+            v-html="getSortIcon('district')"></span></th>
+        <th @click="changeSort('birthday')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Năm sinh<span
+            v-html="getSortIcon('ward')"></span></th>
+        <th @click="changeSort('extra_info')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Thông tin thêm <span
+            v-html="getSortIcon('longitude')"></span></th>
+        <th @click="changeSort('extra_info')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Quyền <span
+            v-html="getSortIcon('longitude')"></span></th>
+        <th @click="changeSort('formattedCreateAt')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Ngày khởi tạo <span
+            v-html="getSortIcon('latitude')"></span></th>
+        <th @click="changeSort('deleted')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Trạng thái <span
+            v-html="getSortIcon('active')"></span></th>
       </template>
 
       <!-- Body Slot -->
       <template #body>
-        <tr v-for="(item, index) in formattedStaffList" :key="index" class="hover:bg-gray-300" @click="updateInventoryModal(item.id)">
+        <tr v-for="(item, index) in formattedStaffList" :key="index" class="hover:bg-gray-300"
+          @click="updateInventoryModal(item.id)">
           <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ index + 1 }}</td>
           <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ item.fullname }}</td>
           <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.email }}</td>
@@ -37,14 +56,13 @@
 
       <template #pagination>
         <div>
-          <Pagination :total-items="pagination.totalElements"
-                      :items-per-page="limit"
-                      :current-page="page+1"
-                      @page-changed="handlePageChange"
-                      @limit-changed="handleLimitChange"></Pagination>
+          <Pagination :total-items="pagination.totalElements" :items-per-page="limit" :current-page="page + 1"
+            @page-changed="handlePageChange" @limit-changed="handleLimitChange"></Pagination>
         </div>
       </template>
     </TableComponent>
+
+    <setRoleComponentVue :id="idAccountSelected" :isOpen="ModalUpdateIsOpen"></setRoleComponentVue>
   </div>
 </template>
 
@@ -54,16 +72,21 @@ import TableComponent from '../table/TableComponent.vue';
 import toggleButton from '../buttons/toggleButton.vue';
 import dayjs from "dayjs";
 
+
+
+import setRoleComponentVue from './setRoleComponent.vue';
+
 export default {
   name: 'listStaffComponent',
   components: {
     TableComponent,
-    toggleButton
+    toggleButton,
+    setRoleComponentVue
   },
   data() {
     return {
-      ModalUpdateInventoryIsOpen: false,
-      idInventorySelected: null,
+      ModalUpdateIsOpen: false,
+      idAccountSelected: null,
       pagination: [],
       staffList: [],
       sortField: 'id',
@@ -78,13 +101,13 @@ export default {
       return this.staffList.map(item => {
         return {
           ...item, // Giữ lại các thông tin khác của item
-          formattedCreateAt: item.createAt 
-            ? dayjs(item.createAt).format("DD/MM/YYYY HH:mm") 
+          formattedCreateAt: item.createAt
+            ? dayjs(item.createAt).format("DD/MM/YYYY HH:mm")
             : "No create date provided"
         };
       });
     }
-  },  
+  },
   mounted() {
     this.loadInventory();
   },
@@ -125,15 +148,15 @@ export default {
       this.page = 0;
       this.loadInventory();
     },
-    // async updateStatusInventory(id){
-    //    try {
-    //    await changeActiveById(id);
-    //    this.loadInventory();
-    //    } catch (error) {
+    // async updateStatusInventory(id) {
+    //   try {
+    //     await changeActiveById(id);
+    //     this.loadInventory();
+    //   } catch (error) {
     //     console.log(error)
-    //    }
-       
-    //     },
+    //   }
+
+    // },
   }
 }
 </script>
---
+++ src/components/staff/setRoleComponent.vue
@@ -0,0 +1,51 @@
+<template>
+
+    <div>
+        <ModalBox :isOpen="isOpen" :loading="isLoading" :closeModal="closeModal" :closeText="'Đóng'">
+            <template #header>
+                <h3 class="sm:text-sm md:text-lg font-bold">Cập nhập quyền cho tài khoản : {{ id }}</h3>
+            </template>
+
+            <template #body>
+                <div>
+                    <label for="role-select" class="font-bold">Chọn quyền:</label>
+                    <select id="role-select" v-model="selectedRoles" multiple>
+                        <option v-for="role in roles" :key="role" :value="role">{{ role }}</option>
+                    </select>
+                </div>
+            </template>
+        </ModalBox>
+    </div>
+</template>
+
+<script>
+import ModalBox from '../modal/ModalBox.vue'
+import { mapGetters } from 'vuex';
+export default {
+    name: 'setRoleComponent',
+    components: {
+        ModalBox
+    },
+    props: {
+        id: {
+            type: String,
+            required: true,
+        },
+        isOpen: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data() {
+        return {
+            selectedRoles: [], // to store selected roles
+            roles: ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_MANAGER'],
+        }
+    },
+    computed: {
+        ...mapGetters('loading', ['isLoading'])
+    },
+}
+</script>
+
+<style></style>
