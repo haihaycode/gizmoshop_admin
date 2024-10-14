3d247b13d6630116628fef66137c190b289fad99
thansex000 <loclop3@gmail.com>
2024-10-14 18:19:11 +0700

    staffList

--- src/components/staff/listStaffComponent.vue
+++ src/components/staff/listStaffComponent.vue
@@ -3,28 +3,49 @@
     <TableComponent>
       <!-- Header Slot -->
       <template #header>
-        <th @click="changeSort('id')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Stt <span v-html="getSortIcon('id')"></span></th>
-        <th @click="changeSort('fullname')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Tên Nhân Viên <span v-html="getSortIcon('inventoryName')"></span></th>
-        <th @click="changeSort('email')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Email <span v-html="getSortIcon('city')"></span></th>
-        <th @click="changeSort('sdt')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Số điện thoại <span v-html="getSortIcon('district')"></span></th>
-        <th @click="changeSort('birthday')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Năm sinh<span v-html="getSortIcon('ward')"></span></th>
-        <th @click="changeSort('extra_info')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Thông tin thêm <span v-html="getSortIcon('longitude')"></span></th>
-        <th @click="changeSort('extra_info')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Quyền <span v-html="getSortIcon('longitude')"></span></th>
-        <th @click="changeSort('formattedCreateAt')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Ngày khởi tạo <span v-html="getSortIcon('latitude')"></span></th>
-        <th @click="changeSort('deleted')" class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Trạng thái <span v-html="getSortIcon('active')"></span></th>
+        <th @click="changeSort('id')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Stt <span
+            v-html="getSortIcon('id')"></span></th>
+        <th @click="changeSort('fullname')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Tên Nhân Viên <span
+            v-html="getSortIcon('inventoryName')"></span></th>
+        <th @click="changeSort('email')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Email <span
+            v-html="getSortIcon('city')"></span></th>
+        <th @click="changeSort('sdt')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Số điện thoại <span
+            v-html="getSortIcon('district')"></span></th>
+        <th @click="changeSort('birthday')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Năm sinh<span
+            v-html="getSortIcon('ward')"></span></th>
+        <th @click="changeSort('extra_info')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Thông tin thêm <span
+            v-html="getSortIcon('longitude')"></span></th>
+        <th @click="changeSort('extra_info')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Quyền <span
+            v-html="getSortIcon('longitude')"></span></th>
+        <th @click="changeSort('formattedCreateAt')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Cập nhật lần cuối<span
+            v-html="getSortIcon('latitude')"></span></th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Cập nhật quyền</th>
+        <th @click="changeSort('deleted')"
+          class="px-6 py-3 text-left text-xs font-medium text-gray-50 uppercase tracking-wider">Trạng thái <span
+            v-html="getSortIcon('active')"></span></th>
       </template>
 
       <!-- Body Slot -->
       <template #body>
-        <tr v-for="(item, index) in formattedStaffList" :key="index" class="hover:bg-gray-300" @click="updateInventoryModal(item.id)">
+        <tr v-for="(item, index) in formattedStaffList" :key="index" class="hover:bg-gray-300"
+          @click="updateInventoryModal(item.id)">
           <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ index + 1 }}</td>
           <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ item.fullname }}</td>
           <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.email }}</td>
           <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.sdt }}</td>
-          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.roles }}</td>
           <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.birthday }}</td>
           <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.extra_info }}</td>
-          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.formattedCreateAt }}</td>
+          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.roles }}</td>
+          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ item.updateAt }}</td>
+          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><i class='bx bxs-edit-alt'></i></td>
           <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
             <toggleButton :is-toggled="item.deleted"></toggleButton>
           </td>
@@ -37,11 +58,8 @@
 
       <template #pagination>
         <div>
-          <Pagination :total-items="pagination.totalElements"
-                      :items-per-page="limit"
-                      :current-page="page+1"
-                      @page-changed="handlePageChange"
-                      @limit-changed="handleLimitChange"></Pagination>
+          <Pagination :total-items="pagination.totalElements" :items-per-page="limit" :current-page="page + 1"
+            @page-changed="handlePageChange" @limit-changed="handleLimitChange"></Pagination>
         </div>
       </template>
     </TableComponent>
@@ -74,17 +92,34 @@ export default {
   },
   computed: {
     formattedStaffList() {
-      // Lặp qua từng phần tử trong mảng staffList và định dạng ngày khởi tạo
       return this.staffList.map(item => {
+        let roles = item.roles;
+        if (Array.isArray(roles)) {
+          roles = roles.map(role => {
+            if (role === "ROLE_ADMIN") {
+              return "Quản trị viên";
+            } else if (role === "ROLE_SHIPPER") {
+              return "Nhân viên giao hàng";
+            } else if (role === "ROLE_STAFF") {
+              return "Nhân viên quản lí";
+            } else if (role === "ROLE_CUSTOMER") {
+              return "Khách hàng";
+            }
+            return role;
+          }).join(', ');
+        }
+
         return {
-          ...item, // Giữ lại các thông tin khác của item
-          formattedCreateAt: item.createAt 
-            ? dayjs(item.createAt).format("DD/MM/YYYY HH:mm") 
+          ...item,
+          roles: roles,
+          updateAt: item.updateAt
+            ? dayjs(item.updateAt).format("DD/MM/YYYY HH:mm")
             : "No create date provided"
         };
       });
     }
-  },  
+  },
+
   mounted() {
     this.loadInventory();
   },
@@ -110,12 +145,13 @@ export default {
     },
     getSortIcon(column) {
       if (this.sortField === column) {
-        return this.sortDirection === 'asc'
+        return this.sortDirection === 'desc'
           ? "<i class='bx bx-sort-a-z'></i>"
           : "<i class='bx bx-sort-z-a'></i>";
       }
       return '';
     },
+
     handlePageChange(newPage) {
       this.page = newPage - 1;
       this.loadInventory();
@@ -132,7 +168,7 @@ export default {
     //    } catch (error) {
     //     console.log(error)
     //    }
-       
+
     //     },
   }
 }
